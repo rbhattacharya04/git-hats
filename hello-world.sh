eb16f6ad5de9494a503cd93296591adfde6ac422
Alexx Perloff <Alexx.Perloff@Colorado.edu>
2020-05-20 11:25:08 -0500

    Return the hello_world.sh return code to 0.

--- hello-world.sh
+++ hello-world.sh
@@ -4,4 +4,5 @@
 echo -e "Running ${0} ...\n================"
 echo -e "Hello world! My name is ${1} ${2}, but most people call me ${3}."
 
-exit -1
+# If the script exits with something other than 0 the GitHub Action will fail.
+exit 0
