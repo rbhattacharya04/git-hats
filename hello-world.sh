4cc3e6538b56bfe3fe596b8d0d935238b5596160
Alexx Perloff <Alexx.Perloff@Colorado.edu>
2020-05-20 11:15:27 -0500

    More environment variable changes.

--- hello-world.sh
+++ hello-world.sh
@@ -2,4 +2,4 @@
 
 
 echo -e "Running ${0} ...\n================"
-echo -e "Hello world! My name is ${1} ${2} ${3}, but most people call me ${4}."
+echo -e "Hello world! My name is ${1} ${2}, but most people call me ${3}."
